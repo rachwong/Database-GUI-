23741a19deabaf971b882737fe42a6dcb0f900b2
rachwong <rachwong.xx@gmail.com>
2017-10-01 19:48:23 +1300

    Managed to input data from database into the table

--- Assignment 3 Real GUI.py
+++ Assignment 3 Real GUI.py
@@ -9,9 +9,6 @@ my_password = "196ff4df"
 my_host = "studdb-mysql.fos.auckland.ac.nz"
 my_database = "stu_rwon253_COMPSCI_280_C_S2_2017"
 
-my_array = [['00','01','02'],
-            ['10','11','12'],
-            ['20','21','22']]
 
 def main():
     app = QApplication(sys.argv)
@@ -28,6 +25,10 @@ class MyWindow(QWidget):
         self.enterButton = QPushButton("Enter")
         self.textbox = QTextEdit()
         self.enterButton.clicked.connect(self.showdata)
+
+        title = "Harry Potter" 
+        my_array = self.query(title)
+
         
         tablemodel = MyTableModel(my_array, self)
         tableview = QTableView()
@@ -55,36 +56,41 @@ class MyWindow(QWidget):
         model=clickedIndex.model()
         
     def showdata(self):
-        data = str(self.query())
-        print(data)
-        self.textbox.setText(data)
-        
-    def query(self):
+##        data = str(self.query(title))
+##        print(data)
+        self.textbox.setText("whatever")
+    
+    def query(self, name):
         conn = mysql.connector.connect(user=my_user, password=my_password,
                                       host=my_host, database=my_database)
         cursor = conn.cursor()
-        sql = "SELECT BOOK_ID, title, author, ISBN, publisher, status FROM BOOKS" 
-        query = " WHERE title LIKE 'Java for Dummies'"
-        fullquery = sql + query
+        sql = "SELECT BOOK_ID, title, author, ISBN, publisher, status FROM BOOKS WHERE title LIKE '%"
+        #query = "Harry Potter"
+        fullquery = sql + name + "%'"
+        full_array = []
         try:
                 cursor.execute(fullquery)
                 rows = cursor.fetchall()
                 #print(rows)
                 for row in rows:
-                        ID = str(row[0])
-                        title = str(row[1])
+                        array = []
+                        ID = row[0]
+                        title = row[1]
                         author = row[2]
                         isbn = row[3]
                         publisher = row[4]
                         status = row[5]
+                        #item = str(ID)+ ' ' + str(title) + ' ' + str(author)
                         #print( "%s, %s, %s, %s, %s, %s" % (ID, title, author, isbn, publisher,status))
-                        return ID, title, author, isbn, publisher, status
-                        #stringdata = ID + title
-                        
-                        
+                        array.append(str(ID))
+                        array.append(str(title))
+                        array.append(str(author))
+                        full_array.append(array)
+                        #return ID, title, author, isbn, publisher, status 
         except:
                 print("Error: unable to fecth data")
 
+        return full_array
         cursor.close()
         conn.close()
 
@@ -92,7 +98,7 @@ class MyTableModel(QAbstractTableModel):
     def __init__(self, datain, parent=None, *args):
         QAbstractTableModel.__init__(self, parent, *args)
         self.arraydata = datain
-        self.header_labels = ['1', '2', '3']
+        self.header_labels = ['ID', 'Title', 'Author']
 
     def rowCount(self, parent):
         return len(self.arraydata)
